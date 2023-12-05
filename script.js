5ddc16db53abd75d0df2d35eaf5f22e048bf6256
bruhoffman <bruhoffman@gmail.com>
2023-12-05 17:22:06 -0300

    Exercícios práticos 1, 2 e 3 feitos

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 // --------------- PRÁTICA GUIADA ---------------
+// PRÁTICA 1
+/* const bimestre1 = [8, 9, 7, 8];
+const bimestre2 = [6, 9, 8, 7];
+const bimestre3 = [10, 9, 7, 8];
+const bimestre4 = [9, 8, 9, 10];
 
+const notasBimestres = [bimestre1, bimestre2, bimestre3, bimestre4];
+ */
 
+/* for (i = 0; i < notasBimestres.length; i++) {
+  let notas = 0;
+  let media = 0;
 
+  for (let j = 0; j < notasBimestres.length; j++) {
+    notas = notas + notasBimestres[i][j];
+  }
+  console.log(`A soma das notas do bimestre ${i + 1} é ${notas}`);
+  console.log(`A média do bimistre ${i + 1} é: ${notas / notasBimestres.length}`);
+} */
 
+// PRÁTICA 2
+/* for (let i in notasBimestres) {
+  let notas = 0;
+  let media = 0;
+  let n = Number(i);
 
+  for (let j = 0; j < notasBimestres.length; j++) {
+    notas = notas + notasBimestres[i][j];
+  }
+  console.log(`A soma das notas do bimestre ${n + 1} é: ${notas}.`);
+  console.log(`A média do bimistre ${n + 1} é: ${notas / notasBimestres.length}`);
+} */
 
+// PRÁTICA 3
+/* for (let i in notasBimestres) {
+  let notas = 0;
+  let media = 0;
+  let n = Number(i);
 
-
-
-
-
-
-
-
-
-
-
-
-
+  for (let nota of notasBimestres[i]) {
+    notas = notas + nota;
+  }
+  console.log(`A soma das notas do bimestre ${n + 1} é: ${notas}.`);
+  console.log(`A média do bimistre ${n + 1} é: ${notas / notasBimestres.length}`);
+} */
 
 // --------------- EXERCÍCIO DE FIXAÇÃO ---------------
 const filmes = [
